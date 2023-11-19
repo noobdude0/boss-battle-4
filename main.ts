644e6a7facdfc20981209bc58357376386ae1626
noobdude0 <146701091+noobdude0@users.noreply.github.com>
2023-11-19 15:19:56 +0530

    character projectiles design changes and a cutscene at beginning

--- main.ts
+++ main.ts
@@ -37,21 +37,21 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     trident = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . . 
-        . . . . f f f f f f f . . . . . 
-        . . f f 4 4 4 4 4 4 4 f f . . . 
-        . f 4 4 4 4 4 4 4 4 4 2 2 f . . 
-        . f 4 4 4 4 2 4 4 4 4 4 4 f . . 
-        f 2 4 4 4 4 4 4 4 4 4 4 4 4 f . 
-        f 4 2 4 4 4 4 4 4 4 2 4 4 4 f . 
-        f 4 4 4 4 4 4 4 4 4 4 4 4 4 f . 
-        f 2 2 2 2 2 2 2 2 2 4 4 4 2 f . 
-        f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . 
-        f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . 
-        f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . 
-        . f 2 2 2 2 2 2 2 2 2 2 2 f . . 
-        . f 2 2 2 2 2 2 2 2 2 2 2 f . . 
-        . . f f 2 2 2 2 2 2 2 f f . . . 
-        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . f 2 2 2 f . . . . . 
+        . . . . . . f 4 4 4 f . . . . . 
+        . . . . . . f 5 5 5 f . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
         `, thePlayer, 100, 0)
 })
 function reverseVelocity (sprite: Sprite) {
@@ -261,6 +261,9 @@ controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
         `, thePlayer, 100, 0)
     pause(200)
 })
+function start () {
+	
+}
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     tentacle = sprites.create(img`
         ................
@@ -350,38 +353,22 @@ let eyeHP = 0
 let statusbar: StatusBarSprite = null
 let thePlayer: Sprite = null
 thePlayer = sprites.create(img`
-    ...........ccccc66666...........
-    ........ccc4444444444666........
-    ......cc444444444bb4444466......
-    .....cb4444bb4444b5b444444b.....
-    ....eb4444b5b44444b44444444b....
-    ...ebb44444b4444444444b444446...
-    ..eb6bb444444444bb444b5b444446..
-    ..e6bb5b44444444b5b444b44bb44e..
-    .e66b4b4444444444b4444444b5b44e.
-    .e6bb444444444444444444444bb44e.
-    eb66b44444bb444444444444444444be
-    eb66bb444b5b44444444bb44444444be
-    fb666b444bb444444444b5b4444444bf
-    fcb666b44444444444444bb444444bcf
-    .fbb6666b44444444444444444444bf.
-    .efbb66666bb4444444444444444bfe.
-    .86fcbb66666bbb44444444444bcc688
-    8772effcbbbbbbbbbbbbbbbbcfc22778
-    87722222cccccccccccccccc22226678
-    f866622222222222222222222276686f
-    fef866677766667777776667777fffef
-    fbff877768f86777777666776fffffbf
-    fbeffeefffeff7766688effeeeefeb6f
-    f6bfffeffeeeeeeeeeeeeefeeeeebb6e
-    f66ddfffffeeeffeffeeeeeffeedb46e
-    .c66ddd4effffffeeeeeffff4ddb46e.
-    .fc6b4dddddddddddddddddddb444ee.
-    ..ff6bb444444444444444444444ee..
-    ....ffbbbb4444444444444444ee....
-    ......ffebbbbbb44444444eee......
-    .........fffffffcccccee.........
-    ................................
+    . . . . . . f f f f f f . . . . 
+    . . . . f f e e e e f 2 f . . . 
+    . . . f f e e e e f 2 2 2 f . . 
+    . . . f e e e f f e e e e f . . 
+    . . . f f f f e e 2 2 2 2 e f . 
+    . . . f e 2 2 2 f f f f e 2 f . 
+    . . f f f f f f f e e e f f f . 
+    . . f f e 4 4 e b f 4 4 e e f . 
+    . . f e e 4 d 4 1 f d d e f . . 
+    . . . f e e e 4 d d d d f . . . 
+    . . . . f f e e 4 4 4 e f . . . 
+    . . . . . 4 d d e 2 2 2 f . . . 
+    . . . . . e d d e 2 2 2 f . . . 
+    . . . . . f e e f 4 5 5 f . . . 
+    . . . . . . f f f f f f . . . . 
+    . . . . . . . f f f . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(thePlayer)
 thePlayer.z = 10
